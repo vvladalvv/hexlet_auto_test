bc881ca308f093b40458873c6c755d78fc4107fc
vlad <centr575@yandex.ru>
2024-06-08 11:53:05 +0300

    reverted the last commit

--- tests/test_capitalize.py
+++
@@ -1,9 +0,0 @@
-from capitalize import capitalize
-
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
-
-print('Все тесты пройдены!')
